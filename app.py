03ac777d70545985d55c9075c309536369a5f6b9
Emanuel181 <emanuel.rusu03@e-uvt.ro>
2023-11-17 22:20:58 +0200

    push

--- app.py
+++ app.py
@@ -2,12 +2,9 @@ from flask import Flask, render_template
 
 app = Flask(__name__)
 
-
 @app.route('/')
-def hello_world():  # put application's code here
+def hello_world():
     return render_template('index.html')
-
-
 
 if __name__ == '__main__':
     app.run()
