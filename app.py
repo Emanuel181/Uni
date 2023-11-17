e45390931076ca8b80fc0f1548803c25a6e8e804
Uni Prowd <unihack235@gmai.com>
2023-11-17 22:12:11 +0200

    push

--- app.py
+++ app.py
@@ -1,11 +1,11 @@
-from flask import Flask
+from flask import Flask, render_template
 
 app = Flask(__name__)
 
 
 @app.route('/')
 def hello_world():  # put application's code here
-    pass
+    return render_template('index.html')
 
 
 
