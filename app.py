75e1e75a027834aa8d38f1725e1ebb0c85c86f1b
Emanuel181 <emanuel.rusu03@e-uvt.ro>
2023-11-18 23:34:08 +0200

    push

--- app.py
+++ app.py
@@ -1,8 +1,10 @@
+import smtplib
+from email.mime.multipart import MIMEMultipart
+from email.mime.text import MIMEText
 from flask import Flask, render_template, request, jsonify
 import overpy
 
 app = Flask(__name__)
-
 @app.route('/')
 @app.route('/home')
 def home():
@@ -35,14 +37,43 @@ def get_nearby_shops():
 def switch_map():
     return render_template('map.html')
 
+def send_email(name, email, phone, message):
+    sender_email = "unihack235@gmail.com"
+    receiver_email = "unihack235@gmail.com"
+    password = "Secretos1"
+
+    msg = MIMEMultipart()
+    msg['From'] = sender_email
+    msg['To'] = receiver_email
+    msg['Subject'] = "New Contact Form Submission"
+
+    body = f"Name: {name}\nEmail: {email}\nPhone: {phone}\nMessage: {message}"
+    msg.attach(MIMEText(body, 'plain'))
+
+    server = smtplib.SMTP('smtp.example.com', 587)  # Replace with your SMTP server
+    server.starttls()
+    server.login(sender_email, password)
+    text = msg.as_string()
+    server.sendmail(sender_email, receiver_email, text)
+    server.quit()
+
 @app.route('/contact', methods=['GET', 'POST'])
 def contact():
+    if request.method == 'POST':
+        name = request.form['name']
+        email = request.form['email']
+        phone = request.form['phone']
+        message = request.form['message']
+
+        # Send an email using the data
+        send_email(name, email, phone, message)
+
     return render_template('contact.html')
+
 
 @app.route("/login")
 def login():
     return render_template('login.html')
-
 
 
 if __name__ == "__main__":
--- static/js/contactFormAnimation.js
+++ static/js/contactFormAnimation.js
@@ -15,15 +15,3 @@ document.addEventListener('DOMContentLoaded', () => {
         container.classList.add('container-animated');
     }, 500); // Delay of 500ms
 });
-
-document.getElementById('contactForm').addEventListener('submit', function(event) {
-    event.preventDefault(); // Prevent the default form submission
-
-    var name = this.name.value;
-    var email = this.email.value;
-    var phone = this.phone.value;
-    var message = this.message.value;
-
-    var mailtoLink = `mailto:unihack235@gmail.com?subject=Contact Form Submission&body=Name: ${name}%0AEmail: ${email}%0APhone: ${phone}%0AMessage: ${message}`;
-    window.location.href = mailtoLink; // Open the mail client with the composed message
-});
