f3ead05f86aea522cdde68d0f224b2210743c967
Uni Prowd <unihack235@gmai.com>
2023-11-18 17:15:31 +0200

    push7

--- static/js/script.js
+++ static/js/script.js
@@ -3,7 +3,7 @@ const registerBtn =document.getElementById('register');
 const loginBtn = document.getElementById('login');
 registerBtn.addEventListener('click',() =>{
     container.classList.add("active");
-})
+});
 loginBtn.addEventListener('click',()=>{
     container.classList.remove('active');
 });
