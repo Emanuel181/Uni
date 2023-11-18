44acc9df915314b5d57fc7d6581c6356f1608599
Emanuel181 <emanuel.rusu03@e-uvt.ro>
2023-11-18 23:48:00 +0200

    push

--- app.py
+++ app.py
@@ -1,10 +1,13 @@
 import smtplib
 from email.mime.multipart import MIMEMultipart
 from email.mime.text import MIMEText
-from flask import Flask, render_template, request, jsonify
+from flask import render_template, request, jsonify
 import overpy
+from flask import Flask
+
 
 app = Flask(__name__)
+
 @app.route('/')
 @app.route('/home')
 def home():
@@ -59,21 +62,43 @@ def send_email(name, email, phone, message):
 
 @app.route('/contact', methods=['GET', 'POST'])
 def contact():
-    if request.method == 'POST':
-        name = request.form['name']
-        email = request.form['email']
-        phone = request.form['phone']
-        message = request.form['message']
-
-        # Send an email using the data
-        send_email(name, email, phone, message)
+    # TO DO:
+    # if request.method == 'POST':
+    #     name = request.form['name']
+    #     email = request.form['email']
+    #     phone = request.form['phone']
+    #     message = request.form['message']
+    #
+    #     # Send an email using the data
+    #     send_email(name, email, phone, message)
 
     return render_template('contact.html')
 
 
-@app.route("/login")
+@app.route('/login', methods=['GET', 'POST'])
 def login():
+    if request.method == 'POST':
+        return render_template('home3.html')
+
     return render_template('login.html')
+
+        # Retrieve the user from the database
+    #     user = User.query.filter_by(username=username).first()
+    #
+    #     # Check if user exists and password is correct
+    #     if user and check_password_hash(user.password, password):
+    #         # Login logic here (e.g., creating a user session)
+    #         return 'Logged in successfully!'
+    #     else:
+    #         # Invalid credentials
+    #         return 'Invalid username or password.'
+    # else:
+    #     # Show the login form
+    #     return render_template('login.html')
+
+@app.route("/register")
+def register():
+    return
 
 
 if __name__ == "__main__":
